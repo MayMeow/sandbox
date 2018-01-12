659cdb3eccabbc7440071e2b6c83e34eaf684054
May <may+github@maymeow.click>
2018-01-12 17:02:37 +0100

    Initial commit

---
+++ src/Controller/Api/PostsController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controller\Api;
+
+use App\Controller\PostsController as BaseController;
+
+/**
+ * Posts Controller
+ *
+ * @property \App\Model\Table\PostsTable $Posts
+ *
+ * @method \App\Model\Entity\Post[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class PostsController extends BaseController
+{
+    //
+}
---
+++ src/Controller/PostsController.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Posts Controller
+ *
+ * @property \App\Model\Table\PostsTable $Posts
+ *
+ * @method \App\Model\Entity\Post[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class PostsController extends AppController
+{
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|void
+     */
+    public function index()
+    {
+        $posts = $this->paginate($this->Posts);
+
+        $this->set(compact('posts'));
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Post id.
+     * @return \Cake\Http\Response|void
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $post = $this->Posts->get($id, [
+            'contain' => []
+        ]);
+
+        $this->set('post', $post);
+    }
+}
