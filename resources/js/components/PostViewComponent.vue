9be105dbd68fb3b0dbc17a6bc4f88692d7f5a311
Hana Song <jungcharlotta@gmail.com>
2018-01-13 18:01:06 +0100

    Update component

--- resources/js/components/PostViewComponent.vue
+++ resources/js/components/PostViewComponent.vue
@@ -14,7 +14,9 @@
         },
         data () {
             return {
-                post: [],
+                post: {
+                    post: []
+                },
                 errors: []
             }
         },
